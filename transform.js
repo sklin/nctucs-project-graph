1f24434f41fbf1db291fc2d6528e0f819eb2448d
林士凱 <sklin@MacBook-Pro.local>
2015-10-18 16:32:44 +0800

    Using jQuery to get JSON data.

--- transform.js
+++ transform.js
@@ -1,4 +1,23 @@
+var genXaxis = function(flow) {
+  x_axis = ['x'];
+  for(var i=0; i<flow.data.length; i++){
+    x_axis.push(i);
+  }
+  return x_axis;
+};
+
+var transform2 = function(flow) {
+  // for data in lctseng's pattern
+  var _data = [];
+  _data.push(flow.name);
+  _data = _data.concat(flow.data);
+
+  return _data;
+};
+
+
 var transform = function(flow) {
+  // for data in lmchen's pattern
   var data = [];
   var name = flow.source_ip + ":" + flow.source_port;
   name += " - " + flow.destination_ip + ":" + flow.destination_port;
